a40ecb202b84e18966d2070be106fd590b3f6aa7
SachaBouk <sachaboukouya@gmail.com>
2025-10-16 13:08:42 +0200

    create new component + starting Toolsview page

---
+++ animflow/src/components/codepen.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="code-editor-wrapper">
+        <!-- <div v-if="!started" class="start-screen">
+            <button @click="startEditor">Try it yourself !</button>
+        </div> -->
+        <div class="code-editor">
+            <div class="code">
+                <form>
+                    <input type="text" name="name" placeholder="Project Name" />
+                    <input type="text" name="description" placeholder="Description" />
+                </form>
+                <div class="html-code">
+                    <h1><img src="./icons/htmlLogo.svg" alt="">HTML</h1>
+                    <textarea name="html"></textarea>
+                </div>
+                <div class="css-code">
+                    <h1><img src="./icons/cssLogo.svg" alt="">CSS</h1>
+                    <textarea name="css"></textarea>
+                </div>
+
+                <div class="controls">
+                    <!-- <button class="back-btn" @click="goBack">↩️ Retour</button> -->
+                    <button @click="runCode">▶️ Run</button>
+                    <button @click="pauseAnimation">⏸ Pause</button>
+                    <button>✓ Create</button>
+                </div>
+            </div>
+            <iframe id="result"></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+    },
+    methods: {
+        startEditor() {
+            this.started = true;
+            this.$nextTick(() => {
+                // On initialise les éléments seulement après l'affichage
+                this.html_code = this.$el.querySelector(".html-code textarea");
+                this.css_code = this.$el.querySelector(".css-code textarea");
+                this.result = this.$el.querySelector("#result");
+
+                this.html_code.value = localStorage.getItem("html_code") || "";
+                this.css_code.value = localStorage.getItem("css_code") || "";
+
+                this.runCode();
+
+                this.html_code.addEventListener("keyup", this.runCode);
+                this.css_code.addEventListener("keyup", this.runCode);
+            });
+        },
+        runCode() {
+            if (!this.result) return;
+            localStorage.setItem("html_code", this.html_code.value);
+            localStorage.setItem("css_code", this.css_code.value);
+
+            const doc = this.result.contentDocument || this.result.contentWindow.document;
+            doc.open();
+            doc.write(`
+        <style>
+          ${this.css_code.value}
+          * {
+            animation-iteration-count: infinite !important;
+          }
+          .paused * {
+            animation-play-state: paused !important;
+            transition: none !important;
+          }
+        </style>
+        <body>
+          ${this.html_code.value}
+        </body>
+      `);
+            doc.close();
+        },
+        pauseAnimation() {
+            const doc = this.result.contentDocument || this.result.contentWindow.document;
+            doc.body.classList.toggle("paused");
+        },
+        goBack() {
+            this.started = false;
+            this.html_code = null;
+            this.css_code = null;
+            this.result = null;
+        }
+    }
+};
+</script>
+
+<style>
+:root {
+    --darkblue: #14007a;
+    --blue: #1a009c;
+    --lightblue: rgb(74, 255, 255);
+    --pink: rgb(255, 40, 113);
+}
+
+.code-editor-wrapper {
+    font-family: sans-serif;
+    /* background-image: linear-gradient(45deg, var(--lightblue), var(--blue)); */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* padding: 2rem; */
+    /* height: 100vh; */
+}
+
+
+/* .start-screen {
+    text-align: center;
+}
+
+.start-screen button {
+    background-color: var(--pink);
+    border: none;
+    color: white;
+    font-size: 1.5rem;
+    padding: 1rem 2rem;
+    border-radius: .5rem;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.start-screen button:hover {
+    background-color: var(--lightblue);
+} */
+
+
+.code-editor {
+    width: 95%;
+    height: 70vh;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: #fff;
+    border-radius: 1rem;
+    overflow: hidden;
+    margin-top: 3rem;
+    margin-bottom: 15rem;
+}
+
+.code {
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    background-color: var(--darkblue);
+    padding: 1rem;
+    border-radius: 0 1rem 1rem 0;
+    overflow-y: hidden;
+}
+
+.code textarea {
+    width: 100%;
+    height: calc(100% - 3rem);
+    background-color: var(--blue);
+    color: var(--lightblue);
+    border: none;
+    padding: 1rem;
+    font-size: 1.1rem;
+    resize: vertical;
+}
+
+.css-code textarea {
+    height: 130%;
+}
+
+.code textarea::-webkit-scrollbar {
+    width: .4rem;
+}
+
+.code textarea::-webkit-scrollbar-thumb {
+    background-color: var(--pink);
+    border-radius: .4rem;
+}
+
+#result {
+    width: 100%;
+    height: 100%;
+    border: none;
+}
+
+h1 {
+    font: 600 1.2rem 'sans-serif';
+    margin: 1rem 0;
+    color: #fff;
+}
+
+h1>img {
+    width: 1.3rem;
+    margin-right: 1rem;
+    vertical-align: middle;
+}
+
+.controls {
+    justify-content: center;
+    text-align: center;
+    margin-top: 8rem;
+}
+
+.controls button {
+    background-color: var(--lightblue);
+    border: none;
+    color: #fff;
+    padding: .7rem 2rem;
+    font-size: 1.1rem;
+    border-radius: .5rem;
+    cursor: pointer;
+    margin: 0 1rem;
+    transition: background-color .3s;
+}
+
+.controls button:hover {
+    background-color: var(--pink);
+}
+
+.controls .back-btn {
+    background-color: var(--pink);
+}
+
+.controls .back-btn:hover {
+    background-color: var(--lightblue);
+}
+</style>
--- animflow/src/views/Toolsview.vue
+++ animflow/src/views/Toolsview.vue
@@ -1,10 +1,48 @@
+<script>
+
+import codepen from '@/components/codepen.vue';
+
+export default {
+  components: {
+    codepen
+  }
+}
+
+</script>
+
 <template>
 
-<div class="HomeStart">
-    <h1>WELCOME</h1>
-    <h1>ON</h1>
-    <h1>TOOLS</h1>
-  </div>
+    <div class="HomeStart">
+        <h1>WELCOME</h1>
+        <h1>ON</h1>
+        <h1>TOOLS</h1>
+    </div>
+
+    <div class="createAnimation">
+        <div id="title">
+            <h2>Create your own animation</h2>
+        </div>
+ 
+            <codepen />
+
+    </div>
 
 </template>
-<style></style>
+
+
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    user-select: none;
+}
+
+h2 {
+    font: 600 4rem 'sans-serif';
+    color: black;
+    margin-top: 3rem;
+    margin-left: 2rem;
+}
+</style>
